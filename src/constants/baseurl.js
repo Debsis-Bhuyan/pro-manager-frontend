ec23eb62b8ae6e3b43d326b6d6c8b07f7c1b3b9a
Debsis-Bhuyan <debasisbhuyan2@gmail.com>
2024-03-18 04:27:18 +0530

    2nd commit

--- src/constants/baseurl.js
+++ src/constants/baseurl.js
@@ -1,6 +1,6 @@
 // export const BACKEND_URL = "https://pro-manage-b.onrender.com/api";
 // export const FRONTEND_URL = "https://pro-manage-f.vercel.app"
 
-export const BACKEND_URL = "http://localhost:4000/api";
+export const BACKEND_URL = "https://pro-manager-backend-96jz.onrender.com/api";
 
-export const FRONTEND_URL = "http://localhost:5173";
+export const FRONTEND_URL = "https://pro-manager-green.vercel.app";
